697e58e3484457aa1acdd2c2a7850346c86a825a
congnh <congnh@mtbit.com>
2020-01-15 16:48:17 +0700

    add progress time play

--- extension/js/app/controller.js
+++ extension/js/app/controller.js
@@ -1,26 +1,65 @@
+let isStart = false;
 module.controller('MainController', [
   '$scope',
-  '$interval',
   'QuizService',
-  function($scope, $interval, QuizService) {
+  function($scope, QuizService) {
     $scope.quizService = QuizService;
-    $scope.determinateValue = 30;
-
-    $interval(function() {
-      $scope.determinateValue += 1;      
-      if ($scope.determinateValue > 100) $scope.determinateValue = 30;       
-    }, 100, 0, true); 
   }
 ]);
 
 module.controller('IndexController', [
   '$scope',
   '$location',
+  '$interval',
   '$anchorScroll',
   'QuizService',
-  function($scope, $location, $anchorScroll, QuizService) {
+  function($scope, $location, $interval, $anchorScroll, QuizService) {
+    // store the interval promise in this variable
+    let promise;
+
     $scope.quiz = QuizService.getQuiz();
     $anchorScroll('top');
+    $scope.quiz.determinateValue = 100;
+
+    // starts the interval
+    $scope.start = function() {
+      // stops any running interval to avoid two intervals running at the same time
+      $scope.stop(); 
+      
+      // store the interval promise
+      if(isStart){
+        promise =  $interval(timeLost, 100, 0, true); 
+      } 
+    };
+    
+    // stops the interval
+    $scope.stop = function() {
+      $interval.cancel(promise);
+    };
+  
+    // starting the interval by default
+    $scope.start(); 
+
+    // stops the interval when the scope is destroyed,
+    // this usually happens when a route is changed and 
+    // the ItemsController $scope gets destroyed. The
+    // destruction of the ItemsController scope does not
+    // guarantee the stopping of any intervals, you must
+    // be responsible for stopping it when the scope is
+    // is destroyed.
+    $scope.$on('$destroy', function() {
+      $scope.stop();
+    });
+
+    function timeLost() {
+      $scope.quiz.determinateValue -= 1;      
+      if ($scope.quiz.determinateValue === 0) {
+        // $scope.quiz.determinateValue = 100
+        console.log('we lost');        
+        let result = -1;    
+        $location.path(`/result/${result}/${$scope.quiz.question.word}`);
+      };       
+    }
 
     $scope.goToResult = (meaning) => {
       let result = 0;
@@ -49,10 +88,16 @@ module.controller('ResultController', [
 
     if ($scope.result) {
       // $scope.text = "Tiếp tục";
+      isStart = true;
       QuizService.increaseScore(1);    
       $location.path(`/`);  
     } else {
-      $scope.text = "Bắt đầu lại";
+      isStart = false;
+      $scope.message = 'So bad!';
+      if($routeParams.result === '-1'){
+        $scope.message = 'Xaxa ur fat!!';
+      }
+      $scope.text = "Restart";
       QuizService.reset();
     }
   }
